080b0e8e31c2dd2ec34b6ae24c4d6d54f07ba7a1
ayush113 <ayush.kumar1999@hotmail.com>
2018-11-05 09:36:50 +0530

    First Commit

---
+++ accounts/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+
+from .views import (
+    LogInView, RemindUsernameView, SignUpView,  LogOutView,
+    ChangeEmailView, ChangeEmailActivateView, ChangeProfileView, ChangePasswordView,
+    RestorePasswordView, RestorePasswordDoneView, RestorePasswordConfirmView,
+)
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('log-in/', LogInView.as_view(), name='log_in'),
+    path('log-out/', LogOutView.as_view(), name='log_out'),
+
+
+    path('sign-up/', SignUpView.as_view(), name='sign_up'),
+
+    path('restore/password/', RestorePasswordView.as_view(), name='restore_password'),
+    path('restore/password/done/', RestorePasswordDoneView.as_view(), name='restore_password_done'),
+    path('restore/<uidb64>/<token>/', RestorePasswordConfirmView.as_view(), name='restore_password_confirm'),
+
+    path('remind/username/', RemindUsernameView.as_view(), name='remind_username'),
+
+    path('change/profile/', ChangeProfileView.as_view(), name='change_profile'),
+    path('change/password/', ChangePasswordView.as_view(), name='change_password'),
+    path('change/email/', ChangeEmailView.as_view(), name='change_email'),
+    path('change/email/<code>/', ChangeEmailActivateView.as_view(), name='change_email_activation'),
+]
