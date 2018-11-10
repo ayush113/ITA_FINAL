38b45fd744d3bf5527a241b6ca196fe3b56a8b8f
ayush113 <ayush.kumar1999@hotmail.com>
2018-11-10 16:24:47 +0530

    Added filesharing

--- ITA_Final/urls.py
+++ ITA_Final/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('home.urls')),
@@ -27,6 +29,12 @@ urlpatterns = [
     path('todolist/',include('todolist.urls')),
     path('posts/',include('posts.urls',namespace='posts')),
     path('comments/',include('comments.urls',namespace='comments')),
-    path('timings/',include('timings.urls'))
+    path('timings/',include('timings.urls')),
+    path('fileshare/',include('filesharing.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
---
+++ filesharing/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from django.db import connection
+from notes.utils import namedtuplefetchall
+
+def simple_upload(request):
+    with connection.cursor() as curr:
+        curr.execute("select filename,shared_url,first_name,last_name from fileshared inner join auth_user on fileshared.user_id = auth_user.id")
+        results = namedtuplefetchall(curr)
+
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        data = request.POST
+        data = data.get('filename')
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        with connection.cursor() as curr:
+            curr.execute("INSERT INTO fileshared(filename,user_id, shared_url) VALUES (%s,%s,%s)",[data,request.user.id,uploaded_file_url])
+        return render(request, 'filesharing/index.html', {
+            'uploaded_file_url': uploaded_file_url,
+            'links':results
+        })
+
+    return render(request, 'filesharing/index.html',{'links':results})
